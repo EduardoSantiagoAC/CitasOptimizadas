cf7a8f2f47b4c3371d4ee96e3a8838be5914643b
JffrGD2 <jeffry50021@gmail.com>
2024-11-21 11:06:53 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask_cors import CORS
 import pyodbc
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app)  # Permitir solicitudes desde el frontend
 
-
+# Conexión a SQL Server
 conn = pyodbc.connect(
     "DRIVER={SQL Server};"
     "SERVER=SANTIAGO12;"
